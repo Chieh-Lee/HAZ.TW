09286af38c71202e95619abbf12a528473ce6f8f
Chieh-Lee <s0143033@gm.ncue.edu.tw>
2019-08-27 15:19:45 +0800

    ver0.1.0 push

--- R/read_fault.R
+++ R/read_fault.R
@@ -1,6 +1,7 @@
 #' @title Read file about fault.src
 #'
 #' @import dplyr
+#' @import here
 #' @import stringr
 #' @import magrittr
 #' @importFrom stats setNames
@@ -69,7 +70,7 @@ read_fault<-function(faultfile){
 
       } else if ( stype == 3 | stype == 4) { #source_type=3|4
         ca[[5]] <- as.character(ca[[5]])
-        Data <- scan(ca[[5]], what = 'c', sep = '\n', allowEscapes = TRUE, quiet = TRUE)
+        Data <- scan(here::here(dirname(faultfile), ca[[5]]), what = 'c', sep = '\n', allowEscapes = TRUE, quiet = TRUE)
         D1_1 <- Data[1:3] #information
         D1_2 <- Data[4:length(Data)]; D1_2 <- unlist(str_split(D1_2, " ")) #location
         D1_3 <- matrix(D1_2, (length(D1_2)/3), 3, byrow = TRUE) %>% data.frame()
